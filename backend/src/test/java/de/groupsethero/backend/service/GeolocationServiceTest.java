5e895fa60594a03ca333a0e3b509d2cbebf3e2de
Joshu4l <albertjoshua5@gmail.com>
2023-10-19 17:10:03 +0200

    remove unused import statement

--- backend/src/test/java/de/groupsethero/backend/service/GeolocationServiceTest.java
+++ backend/src/test/java/de/groupsethero/backend/service/GeolocationServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class GeolocationServiceTest {
 
     // Global Instances for Service-Test-Purposes
